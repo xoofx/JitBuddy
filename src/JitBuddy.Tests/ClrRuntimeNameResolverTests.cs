b77b1434e9e0ddef4ea656479ec7840669c945d3
Ilya Dubadenko <ilya.dubadenko@gmail.com>
2021-04-17 17:03:52 +0300

    Added ClrRuntimeSymbolResolver (#9)

---
+++ src/JitBuddy.Tests/ClrRuntimeNameResolverTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace JitBuddy.Tests
+{
+    [TestFixture]
+    public class ClrRuntimeNameResolverTests
+    {
+        [Test]
+        public void ResolveSymbolTest()
+        {
+            var method = typeof(Foo).GetMethod("FooMethod");
+            var result = method.ToAsm();
+            Console.WriteLine(result);
+            Assert.IsNotEmpty(result);
+            Assert.That(result, Contains.Substring("JitBuddy.Tests.ClrRuntimeNameResolverTests+Bar.BarMethod()"));
+        }
+
+        class Foo
+        {
+            public void FooMethod(Bar b)
+            {
+                b.BarMethod();
+            }
+        }
+
+        class Bar
+        {
+            public void BarMethod()
+            {
+            }
+        }
+    }
+}
